b19851e734fbb8f71d3b26483e09ddac409f4853
juliameleshko <julia.meleshko@yahoo.com>
2014-11-09 17:42:16 +0200

    add AutoMapperConfig Execution in Global.asax

--- AnimalHope/AnimalHope.Web/Global.asax.cs
+++ AnimalHope/AnimalHope.Web/Global.asax.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AnimalHope.Web.Infrastructure.Mapping;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -12,6 +13,8 @@ namespace AnimalHope.Web
     {
         protected void Application_Start()
         {
+            AutoMapperConfig.Execute();
+
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
