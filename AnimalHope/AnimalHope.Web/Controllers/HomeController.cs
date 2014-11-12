798a880fc4017a6a3aed1a4131c57f69b0177d5e
juliameleshko <julia.meleshko@yahoo.com>
2014-11-12 19:23:05 +0200

    Home Page shows all animals

--- AnimalHope/AnimalHope.Web/Controllers/HomeController.cs
+++ AnimalHope/AnimalHope.Web/Controllers/HomeController.cs
@@ -1,16 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
-
-namespace AnimalHope.Web.Controllers
+﻿namespace AnimalHope.Web.Controllers
 {
-    public class HomeController : Controller
+    using AnimalHope.Data;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Web;
+    using System.Web.Mvc;
+    using AutoMapper;
+    using AutoMapper.QueryableExtensions;
+    using AnimalHope.Web.Models;
+
+    public class HomeController : BaseController
     {
+        public HomeController(IApplicationData data)
+            : base(data)
+        {
+        }
+
         public ActionResult Index()
         {
-            return View();
+            var model = this.data.Animals.All()
+                        .Project()
+                        .To<AllAnimalsViewModel>().ToList();
+            return View(model);
         }
 
         public ActionResult About()
---
+++ AnimalHope/AnimalHope.Web/Models/AllAnimalsViewModel.cs
@@ -0,0 +1,62 @@
+﻿namespace AnimalHope.Web.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Web;
+    using AutoMapper;
+    using AnimalHope.Models;
+    using AnimalHope.Web.Infrastructure.Mapping;
+
+    public class AllAnimalsViewModel : IHaveCustomMappings
+    {
+        public AllAnimalsViewModel()
+        {
+            this.Descriptions = new HashSet<Description>();
+        }
+
+        private string picture;
+
+        public int ID { get; set; }
+
+        public string Name { get; set; }
+
+        public AnimalType AnimalType { get; set; }
+
+        public Condition Condition { get; set; }
+
+        public ApplicationUser User { get; set; }
+
+        public IEnumerable<Description> Descriptions { get; set; }
+
+        public string PictureType { get; set; }
+
+        public byte[] Picture { get; set; }
+
+        public string PictureData
+        {
+            get
+            {
+                if (this.picture == null && this.PictureType != null && this.Picture != null)
+                {
+                    this.picture = GetBase64(this.PictureType, this.Picture);
+                }
+
+                return this.picture;
+            }
+        }
+
+        public void CreateMappings(IConfiguration configuration)
+        {
+            configuration.CreateMap<Animal, AllAnimalsViewModel>();
+            //.ForMember(x => x.PictureType, opt => opt.MapFrom(m => m.PictureType))
+            //.ForMember(x => x.Picture, opt => opt.MapFrom(m => m.Picture));
+        }
+        private string GetBase64(string imageType, byte[] imageData)
+        {
+            var binaryContent = Convert.ToBase64String(imageData);
+            var imageBase64 = string.Format("data:{0};base64,{1}", imageType, binaryContent);
+            return imageBase64;
+        }
+    }
+}
