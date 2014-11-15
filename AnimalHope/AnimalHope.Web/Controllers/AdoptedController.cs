8dfd03ede023d7a2ab614203b33a2c95a6cb5a55
juliameleshko <julia.meleshko@yahoo.com>
2014-11-15 22:43:55 +0200

    Adopted animals and their details

---
+++ AnimalHope/AnimalHope.Web/Controllers/AdoptedController.cs
@@ -0,0 +1,33 @@
+﻿namespace AnimalHope.Web.Controllers
+{
+    using AnimalHope.Data;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Web;
+    using System.Web.Mvc;
+    using AutoMapper;
+    using AutoMapper.QueryableExtensions;
+    using AnimalHope.Web.Models;
+
+    [Authorize]
+    public class AdoptedController : BaseController
+    {
+        public AdoptedController(IApplicationData data)
+            : base(data)
+        {
+        }
+
+        [HttpGet]
+        public ActionResult Index()
+        {
+            var model = this.data.Animals
+                .All()
+                .Where(a => a.Condition.Name == "Adopted")
+                .Project()
+                .To<AdoptedViewModel>().ToList();
+
+            return View(model);
+        }
+    }
+}
