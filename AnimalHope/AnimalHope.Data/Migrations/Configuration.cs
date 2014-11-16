1c39741a0955efefd205cc86dd60dc8b1c73897f
juliameleshko <julia.meleshko@yahoo.com>
2014-11-16 14:50:51 +0200

    seeding roles and users

--- AnimalHope/AnimalHope.Data/Migrations/Configuration.cs
+++ AnimalHope/AnimalHope.Data/Migrations/Configuration.cs
@@ -6,6 +6,8 @@ namespace AnimalHope.Data.Migrations
     using System.Linq;
 
     using AnimalHope.Models;
+    using Microsoft.AspNet.Identity;
+    using Microsoft.AspNet.Identity.EntityFramework;
 
     internal sealed class Configuration : DbMigrationsConfiguration<ApplicationDbContext>
     {
@@ -24,24 +26,66 @@ namespace AnimalHope.Data.Migrations
                 return;
             }
 
-            //this.SeedUsers(context);
+            this.SeedRole(context, "Admin");
+            this.SeedRole(context, "User");
+
+            this.SeedAdmin(context, "admin@admin.admin", "123456");
+            this.SeedUser(context, "user@user.user", "123456");
+            
             this.SeedTypes(context);
             this.SeedConditions(context);
 
             context.SaveChanges();
         }
 
-        //private void SeedUsers(ApplicationDbContext context)
-        //{
-        //    context.Users.Add(new ApplicationUser
-        //        {
-        //            Email = "admin@admin.com",
-        //            FirstName = "Admin",
-        //            LastName = "Administrator",
-        //            PhoneNumber = "0888123456",
-        //            Roles = new 
-        //        });
-        //}
+        private void SeedRole(ApplicationDbContext context, string roleName)
+        {
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
+            if (!roleManager.RoleExists(roleName))
+            {
+                roleManager.Create(new IdentityRole(roleName));
+            }
+        }
+
+        private void SeedAdmin(ApplicationDbContext context, string username, string password)
+        {
+            var admin = new ApplicationUser()
+            {
+                UserName = username,
+                Email = username,
+                FirstName = "Admin",
+                LastName = "Administrator",
+                PhoneNumber = "0888123456"
+            };
+
+            var userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
+
+            if (!userManager.Users.Any(u => u.UserName == username))
+            {
+                userManager.Create(admin, password);
+                userManager.AddToRole(admin.Id, "Admin");
+            }
+        }
+
+        private void SeedUser(ApplicationDbContext context, string username, string password)
+        {
+            var admin = new ApplicationUser()
+            {
+                UserName = username,
+                Email = username,
+                FirstName = "User",
+                LastName = "AppUser",
+                PhoneNumber = "0888654321"
+            };
+
+            var userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
+
+            if (!userManager.Users.Any(u => u.UserName == username))
+            {
+                userManager.Create(admin, password);
+                userManager.AddToRole(admin.Id, "User");
+            }
+        }
 
         private void SeedTypes(ApplicationDbContext context)
         {
